86f40a730585a8329ca9c4125436c349cfcc7134
410261311shedy <m12192002@gmail.com>
2025-11-01 07:54:15 +0800

    建立還原點以及練習

---
+++ index.js
@@ -0,0 +1,104 @@
+
+// let count = 0
+// let saveEl = document.getElementById("save-el")
+// let countEl = document.getElementById("count-el")
+
+// function increment() {
+//     count += 1
+//     countEl.textContent = count
+// }
+
+// function save() {
+//     let countStr = count + " - "
+//     saveEl.textContenttStr
+//     countEl.textContent = 0
+//     count = 0
+// }
+//console.log("Let's count people on the subway!")
+
+// let countEl = document.getElementById("count-el")
+// let saveEl = document.getElementById("save-el")
+// let count = 0
+// let entriesCount = 1
+
+// function increment()
+// {
+//     count = count + 1
+//     countEl.innerText= count
+//     console.log(count)
+// }
+
+// function save()
+// {
+//     saveEl.innerText += "\n" + entriesCount + ". " + count 
+//     entriesCount += 1
+//     countEl.textContent = 0
+//     count = 0
+// }
+
+// let welcomeEl = document.getElementById("welcome-el")
+// let name1 = "Shedy"
+// let greeting = "Hi! my name is "
+
+// let myGreeting = greeting + name1
+// welcomeEl.innerText = myGreeting
+
+// welcomeEl.innerText += "😀😀"
+
+
+// let remakeText = document.getElementById("error")
+
+// function remake(){
+//     remakeText.textContent = "Something went wrong, please try again"
+
+// }
+
+// let num1 = 8
+// let num2 = 2
+// let sum = 0
+
+ 
+// document.getElementById("num1-el").textContent = num1
+// document.getElementById("num2-el").textContent = num2
+// document.getElementById("sum-el").textContent
+// let text1 = document.getElementById("sum-el")
+
+// function add() {
+//    text1.textContent = "Sum: "
+//    text1.textContent += num1 + num2
+// }
+// function subtract() {
+//     text1.textContent = "Sum: "
+//     text1.textContent += num1 - num2  
+// }
+// function divide() {
+//     text1.textContent = "Sum: "
+//     text1.textContent += num1/num2 
+// }
+// function multiply() {
+//     text1.textContent = "Sum: "
+//     text1.textContent += num1*num2 
+// }
+
+// let firstCard = 2
+// let secondCard = 9
+
+// let sum = firstCard + secondCard
+
+// if (sum < 21){
+//     console.log("Do you want to draw a new card?")
+// } else if (sum === 21){
+//     console.log("Wohoo! You've got Blackjack!") 
+// } else if (sum > 21){
+//     console.log("You're out of the game!")
+// }
+
+let age = 22
+
+if (age < 21){
+    console.log("you can not enter the club!")
+}
+else {
+    console.log("welcome")
+}
+
---
+++ practice1.js
@@ -0,0 +1,18 @@
+let myPoints = 3
+
+function add3Points(){
+    myPoints += 3
+}
+
+function removePoint(){
+    myPoints -= 1
+}
+
+add3Points()
+add3Points()
+add3Points()
+removePoint()
+removePoint()
+
+console.log(myPoints)
+
